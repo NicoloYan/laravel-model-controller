3ec0053b40c9ad3f7da2b4e338afa90e713f1ec4
NicoloYan <nico.yan@outlook.it>
2022-08-26 16:53:40 +0200

    movie cards printed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('main_content')
-    test
+
+    <h1>Movies</h1>
+
+    @foreach ($movies as $movie)
+        <div>
+            <h3>
+                {{$movie->original_title}}
+            </h3>
+            <span>
+                {{$movie->nationality}}
+            </span>
+            <br>
+            <span>
+                {{$movie->vote}}
+            </span>
+        </div>
+    @endforeach
 @endsection
